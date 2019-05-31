7a8d222304ab9dfadd19c5e6125c30989de782b0
Alex <foxwalkin@gmail.com>
2019-05-31 19:45:06 -0400

    added some test stuff.

--- addon/components/editable-div.js
+++ addon/components/editable-div.js
@@ -2,5 +2,17 @@ import Component from '@ember/component';
 import layout from '../templates/components/editable-div';
 
 export default Component.extend({
-  layout
+  layout,
+  attributeBindings: ['contenteditable'],
+  contenteditable: true,
+  placeholder: '',
+  text: '',
+
+  keyDown(event) {
+    console.log('in keyDown');
+  },
+
+  keyUp() {
+    console.log('in keyUp');
+  }
 });
